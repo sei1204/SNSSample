be69872396e40804ec4b8ff3e30c433acc5873e0
三城聖 <sei.e5hayabusa@gmail.com>
2017-06-28 17:31:04 +0900

    first commit

--- SNSSample/ViewController.swift
+++ SNSSample/ViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override var prefersStatusBarHidden: Bool{
+        return true
+    }
 
+    @IBAction func cameraButtonTapped(sender: AnyObject) {
+        
+        let sourceType: UIImagePickerControllerSourceType = .camera
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let imagePickerController = UIImagePickerController()
+            imagePickerController.sourceType = sourceType
+            imagePickerController.delegate = self
+            self.present(imagePickerController, animated: true, completion: nil)
+        }else {
+            label.text = "カメラは使用できません"
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            imageView.contentMode = .scaleAspectFit
+            imageView.image = image
+        }
+        
+        label.text = "撮影を完了しました"
+        
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+        label.text = "撮影をキャンセルしました"
+    }
+    
 }
 
